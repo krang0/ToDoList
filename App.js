9644c0978fc154110dd4c7df9a20818b0334cda6
Abdullah Arslan <147412768+krang0@users.noreply.github.com>
2025-11-14 15:23:25 +0300

    feat(part2): Add controlled text input and state for tasks

--- App.js
+++ App.js
@@ -1,17 +1,67 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from 'react';
+import { TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredTaskText, setEnteredTaskText] = useState('');
+  const [tasks, setTasks] = useState([]);
+  function taskInputHandler(enteredText) {
+  setEnteredTaskText(enteredText);
+}
+
+function addTaskHandler() {
+  if (enteredTaskText.trim().length === 0) {
+    return;
+  }
+
+  setTasks((currentTasks) => [
+    ...currentTasks,
+    { id: Math.random().toString(), text: enteredTaskText },
+  ]);
+
+  setEnteredTaskText('');
+}
+
   return (
     <SafeAreaView style={styles.appContainer}>
       <View style={styles.contentContainer}>
         <Text style={styles.title}>My Todo List</Text>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.textInput}
+                placeholder="Add a new task..."
+                onChangeText={taskInputHandler}
+                value={enteredTaskText}
+              />
+  <Button title="Add" onPress={addTaskHandler} />
+</View>
+
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  inputContainer: {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  marginBottom: 20,
+  borderBottomWidth: 1,
+  borderColor: '#ccc',
+  paddingBottom: 10,
+},
+textInput: {
+  flex: 1,
+  borderWidth: 1,
+  borderColor: '#ccc',
+  padding: 10,
+  borderRadius: 6,
+  marginRight: 10,
+  fontSize: 16,
+},
+
   appContainer: {
     flex: 1,
     backgroundColor: '#f0f2f5',
